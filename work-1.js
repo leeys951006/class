82b262b564af4b61aa1b278e4e422ace7202e8e1
이연승 <leeys951006@gmail.com>
2024-06-10 10:40:58 +0900

    class

--- work-1.js
+++ work-1.js
@@ -1,1 +1,36 @@
-console.log();
+class Person {
+  constructor() {
+    this._name = '';
+    this._age = 0;
+    this._job = '';
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set job(value) {
+    this._job = value;
+  }
+
+  get job() {
+    return this._job;
+  }
+}
+
+const person = new Person();
+person.name = '공욱재';
+person.age = 26;
+person.job = '미남';
+
+console.log('Name:', person.name);
+console.log('Age:', person.age);
+console.log('Job:', person.job);
