602c7c637188f2d4ab907f43c2e98e6a43cae6b2
이연승 <leeys951006@gmail.com>
2024-06-10 10:56:07 +0900

    work-2.js 내용 추가

--- work-2.js
+++ work-2.js
@@ -1,1 +1,21 @@
-console.log();
+const createPerson = (callback) => {
+  let person = {};
+  callback(person, setName);
+};
+
+const setName = (person, callback) => {
+  person.name = '공욱재';
+  callback(person, setAge);
+};
+
+const setAge = (person, callback) => {
+  person.age = 26;
+  callback(person, setJob);
+};
+
+const setJob = (person) => {
+  person.job = '미남';
+  console.log(person);
+};
+
+createPerson((person, callback) => callback(person, setName));
